4a9fc42c723321bd3ce57ddfcf436808d08abc67
IvashkaVIP <ivashka.co@gmail.com>
2023-04-17 00:02:28 +0300

    task-2a

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayItemEl = ingredients.map(item => {
+  const ingredientsEl = document.createElement("li");
+  ingredientsEl.textContent = item;
+  ingredientsEl.classList.add("item");
+
+  return ingredientsEl;
+});
+
+const ingredientsListEl = document.querySelector('ul#ingredients');
+ingredientsListEl.append(...arrayItemEl);
+
