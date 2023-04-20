7fc78f3cbe051b1f6ef968ba15c95fda71364972
IvashkaVIP <ivashka.co@gmail.com>
2023-04-20 20:38:35 +0300

    task-08

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,8 @@
 const changerFontSize = document.querySelector('#font-size-control');
-const textFontSize = document.querySelector('#text');
 
 changerFontSize.addEventListener('input', onChangeFontSize);
 
 function onChangeFontSize(event) {
-  textFontSize.style.fontSize = `${event.currentTarget.value}px`;
+    const textFontSize = document.querySelector("#text");
+    textFontSize.style.fontSize = `${event.currentTarget.value}px`;
 }
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (!email || !password) {
+        alert('Все поля должны быть заполнены!');
+        return
+    }
+    const formData = { email, password };
+    console.log(formData);  
+    alert ('Данные отправлены')
+    form.reset();
+}
+
+// console.log(form);
