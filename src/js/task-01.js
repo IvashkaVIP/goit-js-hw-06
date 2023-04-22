d925683812f79cc6b810ca07f8ee2bd82fccf680
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-04-22 19:32:28 +0300

    Delete task-01.js

--- src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-const itemEl = document.querySelectorAll("li.item");
-console.log("Number of categories: ", itemEl.length);
-console.log("");
-itemEl.forEach((element) => {
-  console.log("Category: ", element.firstElementChild.textContent);
-  console.log("Elements: ", element.lastElementChild.children.length);
-  console.log("");
-});
